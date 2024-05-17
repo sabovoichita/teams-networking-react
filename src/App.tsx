13414fa9e58bbf96d6111d60716017a4836c0651
Voichita Maria <sabo_voichita@yahoo.com>
2024-05-17 13:25:15 +0200

    addd MainMenu function component

--- src/App.tsx
+++ src/App.tsx
@@ -25,30 +25,35 @@ function AppHeader() {
           </div>
         </div>
       </div>
-
-      <ul id="top-menu-bar">
-        <li>
-          <a href="#" data-page="home">
-            🏡Home
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="skills">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="languages">
-            Languages
-          </a>
-        </li>
-      </ul>
+      <MainMenu />
     </header>
+  );
+}
+
+function MainMenu() {
+  return (
+    <ul id="top-menu-bar">
+      <li>
+        <a href="#" data-page="home">
+          🏡Home
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="skills">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="projects">
+          Projects
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="languages">
+          Languages
+        </a>
+      </li>
+    </ul>
   );
 }
 
